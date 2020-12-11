9a8cfb89481bf628ece2c68c4911012b8bd05eb0
LeeJiaWeiNTU <JLEE224@e.ntu.edu.sg>
2020-12-11 12:42:44 +0800

    Add npm install

---
+++ dbs_seed_app/src/transfer/transfer.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import Transfer from './transfer';
+
+export class Transfer extends Component {
+    render(){
+        if (this.props.account){
+        return(
+            <ul id="transfers" className=
+        <h3>Welcome</h3>    
+        </div>
+    }
+}
