8ecbdc81268542fcce223d8e6471cebcb50420f4
LeeJiaWeiNTU <JLEE224@e.ntu.edu.sg>
2020-12-11 14:50:57 +0800

    Update transferdone.js

--- dbs_seed_app/src/transfer/transferdone.js
+++ dbs_seed_app/src/transfer/transferdone.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 
-import { Col, Divider, Row, Table } from 'antd';
+import { Col, Divider, Row, } from 'antd';
 import 'antd/dist/antd.css';
+
 
 const App = () => {
   return (
-    <div style={{ padding: 20 }}>
+    <div style={{ padding: 50 }}>
       <Row>
         <Col>
-          <Divider>Receipt</Divider>
+          <Divider>Transaction Receipt</Divider>
         </Col>
       </Row>
 
@@ -17,21 +18,25 @@ const App = () => {
         <Col span={20} offset={0}>
           <table>
             <tr>
-              <th>Name :</th>
+              <th>Name: </th>
               <td></td>
             </tr>
+            <p></p>
             <tr>
-              <th>Bank Account No.:</th>
-              <td>123124</td>
+              <th>Bank Account No:</th>
+              <td></td>
             </tr>
+            <p></p>
             <tr>
               <th>Transfer Amount:</th>
               <td>$ </td>
             </tr>
+            <p></p>
             <tr>
-              <th>Transaction No.</th>
+              <th>Transaction No:</th>
               <td></td>
             </tr>
+            <p></p>
             <tr>
               <th>Status:</th>
               <td> </td>
