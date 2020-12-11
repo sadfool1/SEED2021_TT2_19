91e59f41af0eee6f6bba2b7d25ba08b0d30c3d5f
hakim <hakimharith@gmail.com>
2020-12-11 13:59:03 +0800

    controller for login and register

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -23,6 +24,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Express connection success")
+});
+
+app.get('/api/transfer', (req, res) =>{
+    axios.post('https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login',{
+        data: {
+            "username": "Group19",     
+            "password": "SJGsiXX1LfFPyWM" 
+        },
+        headers: {
+            'x-api-key': '1pigYzAdHBepN1i5E1ga2Jdug12i4Mu3Ph8PYp15' 
+        }
+    })
+    .then((response) => {
+        res.json(response.data)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+});
+
+app.get('/login', (req, res) => {
+    axios.get('https://reqres.in/api/login', {
+        data: req.body,
+        headers: {
+            "token": "QpwL5tke4Pnpja7X4"
+            }
+        })
+        .then((response) => {
+            res.json(response.data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 });
 
 const PORT = process.env.PORT || 8080;
