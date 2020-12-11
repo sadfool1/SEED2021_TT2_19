5d50979b36db25c8b2c74347dfd3d5b8e8fbcd1b
James <takw14@gmail.com>
2020-12-11 16:03:02 +0800

    update

---
+++ dbs_seed_app/src/register.js
@@ -0,0 +1,190 @@
+import axios from "axios";
+import "./App.css";
+import React, { useState, useCallback, useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+const apiKey = "1pigYzAdHBepN1i5E1ga2Jdug12i4Mu3Ph8PYp15";
+
+const apiUrl = "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/";
+
+const jwtToken = "mockToken";
+
+const authAxios = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    "x-api-key": `${apiKey}`,
+    Authorization: `Bearer ${jwtToken}`,
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+function App() {
+  const [customerInfo, setCustomerInfo] = useState({
+    custID: "mockID",
+    firstName: "mockFirstName",
+    lastName: "mockLastName",
+    nric: "mockNric",
+    gender: "mockGender",
+    age: "mockAge",
+    phoneNumber: "mockPhoneNumber",
+    email: "mockEmail",
+    address: "mockAddress",
+  });
+  const [errorInfo, setErrorInfo] = useState();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+
+  const classes = useStyles();
+
+  const login = useCallback(async () => {
+    try {
+      //call the api
+
+      const result = await authAxios.post(`/techtrek2020/login`, {
+        username: { username },
+        password: { password },
+        email: { email },
+      });
+
+      setCustomerInfo(result.data);
+    } catch (error) {
+      //catch any errors
+      setErrorInfo(error.message);
+    }
+  }, []);
+
+  const submitHandler = () => {
+    login();
+  };
+
+  return (
+    <div className="App">
+      {/* <button onClick={() => login()}> Login </button>
+
+      <div>
+        {Object.entries(customerInfo).map((info) => {
+          return <ul key={info[1]}>{`${info[0]} : ${info[1]}`}</ul>;
+        })}
+      </div>
+      <p>{errorInfo}</p> */}
+
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Registration
+          </Typography>
+          <form
+            className={classes.form}
+            noValidate
+            onSubmit={(e) => {
+              e.preventDefault();
+              submitHandler();
+            }}>
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="EmailAddress"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="username"
+              label="Username"
+              type="username"
+              id="username"
+              autoComplete="current-username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="red"
+              className={classes.submit}>
+              Sign Up
+            </Button>
+            <Grid container>
+
+
+            </Grid>
+          </form>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
